a889da2dba9ea8b8e1e0b21f64fa1c2dca6b13e1
草鞋没号 <308487730@qq.com>
2021-11-01 20:56:49 +0800

    chore: UPDATE

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from '../package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,9 @@ export default defineConfig({
   build: {
     minify: false,
     outDir: join(process.cwd(), 'dist/react-ts'),
+  },
+  server: {
+    host: pkg.env.HOST,
+    port: pkg.env.PORT,
   },
 })
