9409b56c0dc6e75bda761d8c6142068e0adad337
草鞋没号 <308487730@qq.com>
2021-11-08 19:17:58 +0800

    chore: UPDATE

--- types.d.ts
+++ types.d.ts
@@ -3,5 +3,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production'
     readonly PORT: string
+    readonly HOST: string
+    readonly PORT: number
+    readonly PORT_WS: number
   }
 }
