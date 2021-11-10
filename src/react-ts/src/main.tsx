cb7caa8602413a3b2e3e54c3a13db879d45a20e6
草鞋没号 <308487730@qq.com>
2021-11-10 16:06:38 +0800

    chore: log contextBridge

--- src/react-ts/src/main.tsx
+++ src/react-ts/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+console.log('contextBridge ->', window.bridge)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
