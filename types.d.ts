96e428f8b127e6339b213cf8bed1747934ea8aa3
草鞋没号 <308487730@qq.com>
2021-11-11 17:00:48 +0800

    fix: remove duplicate define

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production'
-    readonly PORT: string
     readonly HOST: string
     readonly PORT: number
   }
