76de9f2b9da4cc5243cb6bc3a59b10e22011a229
草鞋没号 <308487730@qq.com>
2021-11-15 20:00:13 +0800

    chore: Use

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-console.log('contextBridge ->', window.bridge)
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,3 +12,35 @@ ReactDOM.render(
     window.bridge.removeLoading()
   },
 )
+
+// -----------------------------------------------------------
+
+console.log('contextBridge ->', window.bridge)
+
+// Use 'electron-store'
+const store = {
+  async get(key: string) {
+    const { invoke } = window.bridge.ipcRenderer
+    let value = await invoke('electron-store', 'get', key)
+    try {
+      value = JSON.parse(value)
+    } finally {
+      return value
+    }
+  },
+  async set(key: string, value: any) {
+    const { invoke } = window.bridge.ipcRenderer
+    let val = value
+    try {
+      if (value && typeof value === 'object') {
+        val = JSON.stringify(value)
+      }
+    } finally {
+      await invoke('electron-store', 'set', key, val)
+    }
+  },
+}
+
+await store.set('Date.now', Date.now())
+console.log('electron-store ->', 'Date.now:', await store.get('Date.now'))
+console.log('electron-store ->', 'path:', await window.bridge.ipcRenderer.invoke('electron-store', 'path'))
