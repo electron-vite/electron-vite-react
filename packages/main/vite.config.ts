8e702586d5668387fb7bf03b2e826d92ddc13b52
草鞋没号 <308487730@qq.com>
2022-03-01 19:08:06 +0800

    vite-plugin-esm2cjs

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,9 +1,16 @@
 import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
+import esm2cjs from '../../scripts/vite-plugin-esm2cjs'
 import pkg from '../../package.json'
 
 export default defineConfig({
   root: __dirname,
+  plugins: [
+    esm2cjs([
+      'execa',
+      'node-fetch',
+    ]),
+  ],
   build: {
     outDir: '../../dist/main',
     lib: {
