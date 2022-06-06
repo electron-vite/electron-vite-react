0edc38fc9d92be5b999213bba1c535b05f85381a
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2022-06-06 21:23:07 +0300

    feat: use path aliases, improve TS

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/index.css'
+import 'styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
