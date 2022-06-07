4d636614cea56dd86ae2c621bd3900da4dbc6074
草鞋没号 <308487730@qq.com>
2022-06-07 09:49:42 +0800

    fix: debug bootstrap logic

--- .vscode/.debug.script.mjs
+++ .vscode/.debug.script.mjs
@@ -2,13 +2,19 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { createRequire } from 'module'
+import { spawn } from 'child_process'
 
-const pkg = createRequire(import.meta.url)('../package.json')
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const require = createRequire(import.meta.url)
+const pkg = require('../package.json')
 
 // write .debug.env
 const envContent = Object.entries(pkg.env).map(([key, val]) => `${key}=${val}`)
 fs.writeFileSync(path.join(__dirname, '.debug.env'), envContent.join('\n'))
 
 // bootstrap
-import('../scripts/watch.mjs?debug=vscode')
+spawn(
+  process.platform === 'win32' ? 'npm.cmd' : 'npm',
+  ['run', 'dev'],
+  { stdio: 'inherit' },
+)
