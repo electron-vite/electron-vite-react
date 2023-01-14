ac07c369e2145e91ddb159c3e9f427fc0d6d4bdc
草鞋没号 <308487730@qq.com>
2023-01-14 08:46:00 +0800

    chore: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -7,50 +7,52 @@ import { customStart, loadViteEnv } from 'vite-electron-plugin/plugin'
 import renderer from 'vite-plugin-electron-renderer'
 import pkg from './package.json'
 
-rmSync(path.join(__dirname, 'dist-electron'), { recursive: true, force: true })
-
-const isDevelopment = process.env.NODE_ENV === "development" || !!process.env.VSCODE_DEBUG
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, 'src')
-    },
-  },
-  plugins: [
-    react(),
-    electron({
-      include: [
-        'electron'
-      ],
-      transformOptions: {
-        sourcemap: isDevelopment
+export default defineConfig(({ command }) => {
+  rmSync('dist-electron', { recursive: true, force: true })
+
+  const sourcemap = command === 'serve' || !!process.env.VSCODE_DEBUG
+
+  return {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src')
       },
-      plugins: [
-        ...(!!process.env.VSCODE_DEBUG
-          ? [
-            // Will start Electron via VSCode Debug
-            customStart(() => debounce(() => console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App'))),
-          ]
-          : []),
-        // Allow use `import.meta.env.VITE_SOME_KEY` in Electron-Main
-        loadViteEnv(),
-      ],
-    }),
-    // Use Node.js API in the Renderer-process
-    renderer({
-      nodeIntegration: true,
-    }),
-  ],
-  server: !!process.env.VSCODE_DEBUG ? (() => {
-    const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-    return {
-      host: url.hostname,
-      port: +url.port,
-    }
-  })() : undefined,
-  clearScreen: false,
+    },
+    plugins: [
+      react(),
+      electron({
+        include: [
+          'electron'
+        ],
+        transformOptions: {
+          sourcemap,
+        },
+        plugins: [
+          ...(!!process.env.VSCODE_DEBUG
+            ? [
+              // Will start Electron via VSCode Debug
+              customStart(() => debounce(() => console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App'))),
+            ]
+            : []),
+          // Allow use `import.meta.env.VITE_SOME_KEY` in Electron-Main
+          loadViteEnv(),
+        ],
+      }),
+      // Use Node.js API in the Renderer-process
+      renderer({
+        nodeIntegration: true,
+      }),
+    ],
+    server: !!process.env.VSCODE_DEBUG ? (() => {
+      const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+      return {
+        host: url.hostname,
+        port: +url.port,
+      }
+    })() : undefined,
+    clearScreen: false,
+  }
 })
 
 function debounce<Fn extends (...args: any[]) => void>(fn: Fn, delay = 299): Fn {
