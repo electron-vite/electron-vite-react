64b918ab3ca53b011e217fe63d589f6dcc367aa9
Ein <45907846+Ein212@users.noreply.github.com>
2025-08-08 23:57:33 +0800

    Update index.ts
    [fix]bug : ipcRenderer.off is not work

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -2,14 +2,13 @@ import { ipcRenderer, contextBridge } from 'electron'
 
 // --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
-  },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
-  },
+    on(...args: Parameters<typeof ipcRenderer.on>) {
+        const [channel, listener] = args;
+        const listenerWrapper = (event: Electron.IpcRendererEvent, ...args: any[]) =>
+            listener(event, ...args);
+        ipcRenderer.on(channel, listenerWrapper);
+        return () => ipcRenderer.off(channel, listenerWrapper);
+    },
   send(...args: Parameters<typeof ipcRenderer.send>) {
     const [channel, ...omit] = args
     return ipcRenderer.send(channel, ...omit)
